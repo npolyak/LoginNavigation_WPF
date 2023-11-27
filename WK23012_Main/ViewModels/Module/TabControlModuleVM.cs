74010ca160c4db9c1ba4f422c31dafc789763b1c
Carlos J <cjarquin1420@gmail.com>
2023-11-27 13:19:28 +0800

    Module Screen Added
    Model - Main Screen with Tab
    First Tab Done (Main Frame)

---
+++ WK23012_Main/ViewModels/Module/TabControlModuleVM.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using WK23012_Main.Views.ModuleViews;
+using System.Windows.Controls;
+
+namespace WK23012_Main.ViewModels.Module
+{
+    class TabControlModuleVM : ObservableObject
+    {
+        public ObservableCollection<TabItem> TabItems { get; } = new ObservableCollection<TabItem>();
+
+        public TabControlModuleVM()
+        {
+            AddTab("Main Frame", new MainFrame());
+            AddTab("Clip Reel", new ClipReel());
+            AddTab("AMS_BF", new AMS_BF());
+            AddTab("OVI", new OVI());
+            //AddTab("Vacuum Profile", new VacuumProfile());
+        }
+
+        private void AddTab(string header, UserControl content)
+        {
+            TabItems.Add(new TabItem { Header = header, Content = content });
+        }
+    }
+}
