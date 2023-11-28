5f7ad879f641d7a9526770f49acaa2948b785544
Carlos J <cjarquin1420@gmail.com>
2023-11-28 13:29:03 +0800

    NumericUpDown added

--- WK23012_Main/Views/ModuleViews/ClipReel.xaml.cs
+++ WK23012_Main/Views/ModuleViews/ClipReel.xaml.cs
@@ -1,23 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WK23012_Main.Views.ModuleViews
 {
-    /// <summary>
-    /// Interaction logic for ClipReel.xaml
-    /// </summary>
     public partial class ClipReel : UserControl
     {
         public ClipReel()
---
+++ WK23012_Main/Views/ModuleViews/numericUpDown.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WK23012_Main.Views.ModuleViews
+{
+    public partial class numericUpDown : UserControl
+    {
+        public numericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private void IncreaseValue(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(NumericTextBox.Text, out int value))
+            {
+                value++;
+                NumericTextBox.Text = value.ToString();
+            }
+        }
+
+        private void DecreaseValue(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(NumericTextBox.Text, out int value) && value > 0)
+            {
+                value--;
+                NumericTextBox.Text = value.ToString();
+            }
+        }
+
+    }
+}
