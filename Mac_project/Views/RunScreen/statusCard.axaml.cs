4d9414bd6e4165c08a25bafd4f8886b446e41927
pmeneses <pedro200994@outlook.com>
2023-12-03 17:33:38 -0800

    creamos pantalla principal de run screen
    creamos pantalla principal de run screen
    
    aun estan pendiente la seccion de error message

---
+++ Mac_project/Views/RunScreen/statusCard.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mac_project.Views.RunScreen;
+
+public partial class statusCard : UserControl
+{
+    public statusCard()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Mac_project/Views/RunScreen/vacuumOvenStatusList.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Mac_project.ViewModels;
+
+namespace Mac_project.Views.RunScreen;
+
+public partial class vacuumOvenStatusList : UserControl
+{
+    public vacuumOvenStatusList()
+    {
+        InitializeComponent();
+        DataContext = new RunScreenViewModel();
+    }
+}
