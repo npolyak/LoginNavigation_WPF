1ba73faf00218e23cb112c592c1d83d0b656404c
cjarquin1420 <cjarquin1420@gmail.com>
2023-12-07 17:48:39 +0800

    Adding Module Screen
    Main Frame
    Clip Reel

---
+++ Mac_project/Views/Module/Clip Reel.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mac_project.Views.Module;
+
+public partial class Clip_Reel : UserControl
+{
+    public Clip_Reel()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Mac_project/Views/Module/MainFrameClipReelOVI.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mac_project.Views.Module;
+
+public partial class MainFrameClipReelOVI : UserControl
+{
+    public MainFrameClipReelOVI()
+    {
+        InitializeComponent();
+    }
+}
