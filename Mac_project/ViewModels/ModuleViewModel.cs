d8513b814e4e3cde0e4b7d8bd7bc2c3b2fc659e8
cjarquin1420 <cjarquin1420@gmail.com>
2023-12-08 18:05:44 +0800

    ASM_BF, OVI and MO added
    Adding ASM_BF, OVI and MO screens

--- Mac_project/ViewModels/ModuleViewModel.cs
+++ Mac_project/ViewModels/ModuleViewModel.cs
@@ -1,7 +1,13 @@
-﻿
+﻿using System.Collections.ObjectModel;
+using System;
+
 namespace Mac_project.ViewModels
 {
     public class ModuleViewModel : ViewModelBase
     {
+        private readonly ObservableCollection<string> _errorMessages;
+        public string errorMessages => String.Join("\r\n", _errorMessages);
+
+
     }
 }
