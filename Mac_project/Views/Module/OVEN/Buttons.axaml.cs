e86b7d274b4ea9a245bba2a3b51aff99895ec619
cjarquin1420 <cjarquin1420@gmail.com>
2023-12-22 17:38:52 +0800

    Adding Oven screen

---
+++ Mac_project/Views/Module/OVEN/Buttons.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mac_project.Views.Module.OVEN;
+
+public partial class Buttons : UserControl
+{
+    public Buttons()
+    {
+        InitializeComponent();
+    }
+}
