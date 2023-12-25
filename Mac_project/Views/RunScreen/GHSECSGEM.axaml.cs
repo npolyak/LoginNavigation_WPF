69d47c44a11e593f6d47e913d1733cd30c73f335
cjarquin1420 <cjarquin1420@gmail.com>
2023-12-25 17:27:39 +0800

    Oven Screen Completed
    - Oven Done
    - Adding other screens for Run Screen

---
+++ Mac_project/Views/RunScreen/GHSECSGEM.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Mac_project.Views.RunScreen;
+
+public partial class GHSECSGEM : UserControl
+{
+    public GHSECSGEM()
+    {
+        InitializeComponent();
+    }
+}
